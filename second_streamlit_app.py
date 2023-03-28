aaa1a164e3cec4a7a8e6dc14d069fddc3ba1c291
jwcourse <128603105+jwcourse@users.noreply.github.com>
2023-03-28 19:49:58 +0100

    Update second_streamlit_app.py

--- second_streamlit_app.py
+++ second_streamlit_app.py
@@ -6,8 +6,8 @@ import snowflake.connector
 
 def run_a_query(Query1):
   with my_cnx.cursor() as my_cur:
-   my_cur.execute(Query1)
-   return my_cur.fetchall()
+    my_cur.execute(Query1)
+    return my_cur.fetchall()
 
 Beta = run_a_query("select color_or_style from catalog_for_website")
 df = pd.DataFrame(Beta)
