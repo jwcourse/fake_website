b21ddfb3670e32749cb0c2539cb239ddd904504a
jwcourse <128603105+jwcourse@users.noreply.github.com>
2023-03-28 21:36:54 +0100

    Update second_streamlit_app.py

--- second_streamlit_app.py
+++ second_streamlit_app.py
@@ -6,7 +6,7 @@ import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * FROM CATALOG_FOR_WEBSITE")
+my_cur.execute("SELECT * FROM SWEATSUITS")
 my_catalog = my_cur.fetchall() 
 
 
