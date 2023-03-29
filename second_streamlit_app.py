20d76b7ff33afa893c060250218d12b92b7c77e4
jwcourse <128603105+jwcourse@users.noreply.github.com>
2023-03-29 21:46:49 +0100

    Update second_streamlit_app.py

--- second_streamlit_app.py
+++ second_streamlit_app.py
@@ -11,18 +11,22 @@ my_catalog = my_cur.fetchall()
 
 
 df = pd.DataFrame(my_catalog)
-#streamlit.write(df)
+
 
 color_list = df[0].values.tolist()
 
 option = streamlit.selectbox('Pick a sweatsuit color or style:', list(color_list))
 product_caption = 'Our warm, comfortable, ' + option + ' sweatsuit!'
 
+
+
 query = "SELECT direct_url, price, size_list, upsell_product_desc FROM catalog_for_website WHERE color_or_style = %s;"
 my_cur.execute(query, (option,))
 df2 = my_cur.fetchone()
-streamlit.image(
-df2[0],
-width=400,
-caption= product_caption
-)
+
+streamlit.write(df2)
+#streamlit.image(
+#df2[0],
+#width=400,
+#caption= product_caption
+#)
