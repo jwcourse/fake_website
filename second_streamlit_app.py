3e7b90e7ed455ca80ad4c7a5f83d756028ec3ba7
jwcourse <128603105+jwcourse@users.noreply.github.com>
2023-03-29 21:50:02 +0100

    Update second_streamlit_app.py

--- second_streamlit_app.py
+++ second_streamlit_app.py
@@ -23,9 +23,8 @@ my_cur.execute("select direct_url, price, size_list, upsell_product_desc from ZE
 
 df2 = my_cur.fetchone()
 
-streamlit.write(df2)
-#streamlit.image(
-#df2[0],
-#width=400,
-#caption= product_caption
-#)
+#streamlit.write(df2)
+streamlit.image(
+df2[0],
+width=400,
+caption= product_caption)
